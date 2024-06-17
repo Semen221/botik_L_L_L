5d829e021d00d1e6cd1989e03e4323073674025e
Semen221 <ifon6marianna@yandex.ru>
2024-06-17 19:59:05 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -11,13 +12,25 @@ async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 @bot.command()
-async def hello(ctx):
+async def hi(ctx):
     await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send(f'Bye!')
+
+@bot.command()
+async def random(ctx, left: int, right: int):
+    await ctx.send(random.randint(left, right))
 
 @bot.command()
 async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)
+
+@bot.command()
+async def minus(ctx, left: int, right: int):
+    await ctx.send(left - right)
 
 @bot.command()
 async def repeat(ctx, times: int, content='repeating...'):
