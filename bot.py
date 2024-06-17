ea7af6d178dbfbaff7c851f8cc8c33a7a9a48631
Semen221 <ifon6marianna@yandex.ru>
2024-06-17 20:21:00 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,33 +13,42 @@ async def on_ready():
 
 @bot.command()
 async def hello(ctx):
+    """Bot will say hello to you"""
     await ctx.send(f'Привет! Я бот {bot.user}!')
 
 @bot.command()
 async def bye(ctx):
+    """Bot will say bye to you"""
     await ctx.send(f'Bye!')
 
 @bot.command()
-async def random(ctx, left: int, right: int):
+async def rand0m(ctx, left: int, right: int):
+    """Choose a random number (type: $random <number1> <number2>)"""
     await ctx.send(random.randint(left, right))
 
 @bot.command()
 async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
+    """Adds two numbers together (type: $add <number1> <number2>)"""
     await ctx.send(left + right)
 
 @bot.command()
 async def minus(ctx, left: int, right: int):
+    """Deducts from number1 number2 (type: minus <number1> <number2>)"""
     await ctx.send(left - right)
 
 @bot.command()
 async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
+    """Repeats a message multiple times (type: $repeat <number>)"""
     for i in range(times):
         await ctx.send(content)
 
 @bot.command()
 async def heh(ctx, count_heh = 5):
+    """Say he (type: $heh <number>)"""
     await ctx.send("he" * count_heh)
+
+
+async def help(ctx):
+    await ctx.send('$hello, $bye, $rand0m <число1> <число2>, $add <число1> <число2>, $minus <число1> <число2>, $repeat <число>, $heh <число>')
 
 bot.run("Token")
